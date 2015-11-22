3d98310807e651f8476b17932b3986e1012b875b
zenithtekla <zenithtekla@gmail.com>
2015-11-22 18:33:08 +0000

    r33 refine ui_template
    - fix minor glitch in the UI
    - added some ui_data
    - employed ui_template for CMOD interface

--- model/ui/ui_data.js
+++ model/ui/ui_data.js
@@ -1,5 +1,3 @@
-var jsonData = JSON.parse(localStorage.getItem("sessionData"));
-
 var tpl_data = {
 	app_name: "SerialScan",
 	app_name_ext1: " USER ACCESS",
@@ -28,6 +26,8 @@ var tpl_data = {
 	lang_022:"Query ",
 	lang_023:" Search",
 	lang_024:" display fetching queryResult",
+	lang_025:" New Format",
+	lang_026:" CMOD ENTER",
 	sample_code: ` -- SQL Code Sample
 					CREATE TABLE IF NOT EXISTS 'seriscan_format' (
 						'id' int(11) NOT NULL AUTO_INCREMENT,
@@ -39,20 +39,33 @@ var tpl_data = {
 	logo_size: "border:0;max-width:40px;max-height:40px;",
 	logo_file: "../img/serialscan.png",
 	ent: "Express Manufacturing Inc.",
-	cmod_link: '<a href="cmod_console.php"><span class="label label-default no-print"><span class="glyphicon glyphicon-cog"></span>&nbspCMOD&nbsp<span class="glyphicon glyphicon-barcode"></span>&nbsp<span class="glyphicon glyphicon-qrcode"></span></span></a>',
-	tpl_dataObj:{
-		primary_key: jsonData.primary_key,
-	    assembly: jsonData.assembly,
-	    revision: jsonData.revision,
-	    customer: jsonData.customer,
-	    sale_order: jsonData.sale_order,
-	    format: jsonData.format,
-	    format_example: jsonData.format_example,
-	    access_level: jsonData.access_level,
-	    username: jsonData.username,
-	    time: jsonData.time,
-	    sessionId: "sessionId: " + jsonData.sessionId
-	}
+	cmod_link: '<a href="cmod_console.php"><span class="label label-default no-print"><span class="glyphicon glyphicon-cog"></span>&nbspCMOD&nbsp<span class="glyphicon glyphicon-barcode"></span>&nbsp<span class="glyphicon glyphicon-qrcode"></span></span></a>'
 };
+
+// object["property"] = value;
+// object.property = value;
+
+// Retrieve dynamic data
+if (localStorage.getItem("sessionData")){
+	var jsonData = JSON.parse(localStorage.getItem("sessionData"));
+	var tpl_data2 = {
+		tpl_dataObj:{
+			primary_key: jsonData.primary_key,
+		    assembly: jsonData.assembly,
+		    revision: jsonData.revision,
+		    customer: jsonData.customer,
+		    sale_order: jsonData.sale_order,
+		    format: jsonData.format,
+		    format_example: jsonData.format_example,
+		    access_level: jsonData.access_level,
+		    username: jsonData.username,
+		    time: jsonData.time,
+		    sessionId: "sessionId: " + jsonData.sessionId
+		}
+	};
+		// if (jsonData.length)
+		// Merge tpl_data2 into tpl_data
+		$.extend( tpl_data, tpl_data2 );
+}
 
 localStorage.setItem("tpl_data", JSON.stringify(tpl_data));
--- view/cmod_enter.php
+++ view/cmod_enter.php
@@ -1,30 +1,54 @@
 <head>
 <link href="../css/default.css" rel="stylesheet">
 <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-
 <script>
   function preventBack(){window.history.forward();}
   setTimeout("preventBack()", 0);
   window.onunload=function(){null};
-  
   // intercept form submit and use Ajax http://stackoverflow.com/a/4069359
 </script>
 
 </head>
-<?php
-echo '
-	<br/>
+<body>
+<section id="ui_data"></section>
+<script type="text/template" id="ui_template">
 	<div class="container" id="printable">
-	<h2>CMOD ENTER</h2>
-	<br/>
-	<form action="../controller/cmod_enter_post.php" method="post">
-		<p>customer_name <span class="required">*</span>: 	<input type="text" name="customer_name"></p>
-		<p>assembly_number <span class="required">*</span>: 	<input type="text" name="assembly_number"></p>
-		<p>revision <span class="required">*</span>: 			<input type="text" name="revision"></p>
-		<p>format <span class="required">*</span>: 			<input type="text" name="format"></p>
-		<p>format_example <span class="required">*</span>: 			<input type="text" name="format_example"></p>
-		<input type="submit" value="New Format"> <input type="reset" class="marginleft" value="Reset">
-	</form>
+		<h2>{{lang_026}}</h2>
+		<br/>
+		<form action="../controller/cmod_enter_post.php" method="post">
+			<p>{{lang_014}} {{required}} 	<input type="text" name="customer_name"></p>
+			<p>{{lang_008}} {{required}} 	<input type="text" name="assembly_number"></p>
+			<p>{{lang_009}} {{required}} 	<input type="text" name="revision"></p>
+			<p>{{lang_011}} {{required}} 	<input type="text" name="format"></p>
+			<p>{{lang_012}} {{required}} 	<input type="text" name="format_example"></p>
+
+			<input type="submit" value="{{lang_025}}"> <input type="reset" class="marginleft" value="{{lang_018}}">
+		</form>
 	</div>
-';
+</script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="../plugin/handlebars/handlebars-v4.0.4.js"></script>
+<script src="../model/ui/ui_data.js" type="text/javascript"></script>
+<script type="text/javascript">
+(function(){
+    var content = document.getElementById('ui_data');
+
+    var data = JSON.parse(localStorage.getItem("tpl_data"));
+
+    Handlebars.registerHelper('bold',function(text){
+        text = Handlebars.escapeExpression(text);
+       return new Handlebars.SafeString('<b>'+text+'</b>');
+    });
+    Handlebars.registerHelper('italic',function(text){
+        text = Handlebars.escapeExpression(text);
+       return new Handlebars.SafeString('<i>'+text+'</i>');
+    });
+    Handlebars.registerHelper("required", function(){
+			return new Handlebars.SafeString('<span class="required"> * </span>');
+		});
+
+    var template = Handlebars.compile(document.getElementById('ui_template').innerHTML);
+    content.innerHTML += template(data);
+})();
+</script>
+</body>
